db6664c449335d51d6f7b0e1b61590bb4e684c01
RomyCardozo <romycardozopy@gmail.com>
2024-10-14 18:38:45 -0300

    añadido @EnableAspectJAutoProxy

--- src/main/java/com/salonViky/SalonBellezaVikyApplication.java
+++ src/main/java/com/salonViky/SalonBellezaVikyApplication.java
@@ -3,10 +3,12 @@ package com.salonViky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableAspectJAutoProxy
 
 public class SalonBellezaVikyApplication {
 
